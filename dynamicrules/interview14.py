8e136818651053866ee2fec726955a81ee22a52b
TechInTech <wdw_bluesky@163.com>
2019-04-22 09:12:34 +0800

    update the file

--- dynamicrules/interview14.py
+++ dynamicrules/interview14.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math
 
 def maxproduct(length):
     if length < 2:
@@ -27,9 +27,24 @@ def maxproduct(length):
     return max
 
 
+def maxproduct2(length):
+    if length < 2:
+        return 0
+    if length == 2:
+        return 1
+    if length == 3:
+        return 2
+
+    timesof3 = length // 3
+    if (length - timesof3 * 3) == 1:
+        timesof3 -= 1
+    timesof2 = (length - timesof3*3) // 2
+    return (math.pow(3, timesof3)) * (math.pow(2, timesof2))
+
 def main():
     length = int(input('Please input the length of string:'))
-    maxpro = maxproduct(length)
+    # maxpro = maxproduct(length)
+    maxpro = maxproduct2(length)
     print('When the string\'s length is %d, the maximum product is %d' % (length, maxpro))
 
 
