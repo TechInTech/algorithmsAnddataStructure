04a010bae6a08029485a6c1867f1806b158576e0
TechInTech <wdw_bluesky@163.com>
2019-04-25 20:23:47 +0800

    Add binarytree to tree

---
+++ tree/interview26.py
@@ -0,0 +1,72 @@
+# -*- coding:utf-8 -*-
+
+from binarytree import BinaryTreeNode
+from tree import Tree
+
+
+class Solution_26(object):
+
+    def has_subtree(self, tree1, tree2):
+        result = False
+
+        if tree1 is not None and tree2 is not None:
+            if self.isequal(tree1.data_, tree2.data_):
+                result = self.tree1_has_tree2(tree1, tree2)
+            if not result:
+                result = self.has_subtree(tree1.left_, tree2)
+            if not result:
+                result = self.has_subtree(tree1.right_, tree2)
+        return result
+
+    def tree1_has_tree2(self, tree1, tree2):
+        if tree2 is None:
+            return True
+
+        if tree1 is None:
+            return True
+
+        if not self.isequal(tree1.data_, tree2.data_):
+            return False
+
+        return self.tree1_has_tree2(tree1.left_, tree2.left_) and self.tree1_has_tree2(tree1.right_, tree2.right_)
+
+    def isequal(self, num1, num2):
+        # if (num1 - num2 > -0.0000001) and (num1 - num2 < 0.0000001):
+        if (num1 == num2):
+            return True
+        else:
+            return False
+
+
+def main():
+    # 由前序遍历和中序遍历产生树
+    preo = [1, 2, 4, 7, 3, 5, 6, 8]
+    ino = [4, 7, 2, 1, 5, 3, 8, 6]
+    length = len(preo)
+    cls_Tree = Tree()
+    cls_Tree.gene_tree(preo, ino, length)
+    main_tree = cls_Tree.get_tree()
+    cls_Tree.travel_tree(main_tree)
+
+    print('\n\n****************subtree*******************\n')
+
+    # 子树
+    preo = [3, 5, 6]
+    ino = [5, 3, 6]
+    length = len(preo)
+    cls_sub_Tree = Tree()
+    cls_sub_Tree.gene_tree(preo, ino, length)
+    sub_tree = cls_sub_Tree.get_tree()
+    cls_sub_Tree.travel_tree(sub_tree)
+
+    decide = Solution_26()
+    result = decide.has_subtree(main_tree, sub_tree)
+    print('\n')
+    if result:
+        print('The subtree belongs to tree!')
+    else:
+        print('The subtree do not belong to tree!')
+
+
+if __name__ == '__main__':
+    main()
--- tree/tree.py
+++ tree/tree.py
@@ -1,20 +1,23 @@
 # -*- coding:utf-8 -*-
 
-from BinaryTreeNode import BinaryTreeNode
+from binarytree import BinaryTreeNode
+
 
 class Tree(object):
     def __init__(self):
-        pass
+        self.root = None
 
     def gene_tree(self, preorder, inorder, length):
         """Generate the tree by the preorder sequence, inorder sequence and the length of sequence.
         """
-        if preorder == [] or inorder == [] or length <= 0:
+        if len(inorder) == 0 or length == 0:
             return -1
         fro = 0
         rear = length - 1
-        return self.constructtree(preorder, inorder, fro, rear, fro, rear)
+        self.root = self.constructtree(preorder, inorder, fro, rear, fro, rear)
 
+    def get_tree(self):
+        return self.root
 
     def constructtree(self, preorder, inorder, k1, Lk1, k2, Lk2):
         rootval = preorder[k1]
@@ -45,12 +48,11 @@ class Tree(object):
                 preorder, inorder, leftpreo + 1, Lk1, rootino + 1, Lk2)
         return root
 
-
     def travel_tree(self, tree):
-        # print(tree.data_)    # preorder travel
+        print(tree.data_, end=', ')    # preorder travel
         if tree.left_ is not None:
             self.travel_tree(tree.left_)
-        print(tree.data_)    # inorder travel
+        # print(tree.data_)    # inorder travel
         if tree.right_ is not None:
             self.travel_tree(tree.right_)
         # print(tree.data_)    # postorder travel
