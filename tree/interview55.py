4c8e80be80b447c378b4adfcff44d1387ad0146a
TechInTech <wdw_bluesky@163.com>
2019-05-15 20:08:02 +0800

    二叉树的深度

---
+++ tree/interview55.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+"""二叉树的深度
+"""
+
+class SOlution_55(object):
+    def tree_depth(self, tree_root):
+        if not tree_root:
+            return 0
+
+        left = self.tree_depth(tree_root.left_)
+        right = self.tree_depth(tree_root.right_)
+
+        return (left + 1) if left > right else (right + 1)
+
+from generate_tree import Generate_tree
+
+def main():
+    tree = Generate_tree()
+    tree_root = tree.create_tree()
+    tree.print_structure()
+
+    depth = SOlution_55().tree_depth(tree_root)
+    print('The depth of tree is %d.'%depth)
+
+
+if __name__ == '__main__':
+    main()
